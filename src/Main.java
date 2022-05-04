4ebfd29cfc832f76c1192eac1b194eae10aa12c5
unknown <philkins2014@mail.ru>
2022-05-04 21:32:19 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int replenishment = 1100;
+        int bonus = replenishment / 100;
+
+        if (replenishment >= 1000) {
+            balance = balance + bonus + replenishment;
+        }
+        else {
+            balance = balance + replenishment;
+        }
+        System.out.println(balance);
+    }
+}
